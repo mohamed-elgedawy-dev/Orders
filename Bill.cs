c98f2014acc186f89ef93c97b851ff904dbdc587
mohamed elgedawy <mohamed.elgedawy100@gmail.com>
2025-10-01 08:11:07 +0300

    Add project files.

---
+++ Bill.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Orders
+{
+
+    
+
+
+    internal class Bill
+    {
+
+        public List<BillItem> BillList { get; set; } = new List<BillItem>(); 
+
+
+   
+
+
+
+
+    }
+}
---
+++ BillSave.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Orders
+{
+    internal class BillSave
+    {
+        private Bill bill;
+        public BillSave()
+        {
+            
+        }
+        public BillSave(List<BillItem> list)
+        {
+            string customerName = list[0].CustomerName ?? "Unknown";
+
+            
+            string fileName = $"{customerName}.json";
+
+
+            string json = JsonSerializer.Serialize(list, new JsonSerializerOptions { WriteIndented = true });
+
+            
+            File.WriteAllText(fileName, json);
+
+
+
+        }
+
+     
+    }
+}
